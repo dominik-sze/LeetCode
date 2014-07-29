77fa9c3ff14262406f0f91e39b77eb4dcd175c3c
Dominik Szerszen <dominik.sze@gmail.com>
2014-07-29 11:26:23 +0200

    Solved RemoveDuplicatesFromSortedList in java

---
+++ RemoveDuplicatesFromSortedList/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode theHead = head;
+        
+        while(head!=null && head.next!=null) {
+            if(head.val==head.next.val) {
+                head.next = head.next.next;
+            } else {
+                head = head.next;
+            }
+        }
+        return theHead;
+    }
+}
