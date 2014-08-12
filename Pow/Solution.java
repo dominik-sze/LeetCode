888c6ee44cfaa82378752f51d39ed43edb62f8ff
Dominik Szerszen <dominik.sze@gmail.com>
2014-08-12 14:09:03 +0200

    Solved Pow(x,n) in java

---
+++ Pow/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public double pow(double x, int n) {
+        if(n==0) return 1;
+        if(n==1) return x;
+        if(x==0) return x;
+        if(n<0) {
+            x = 1/x;
+            n = -n;
+        }
+        if(n%2!=0) {  
+            return x*pow(x*x, n/2);
+        } else {        
+            return pow(x*x, n/2);
+        }
+    }
+}
