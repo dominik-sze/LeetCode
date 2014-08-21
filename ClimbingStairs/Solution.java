001c7373a524d4e775680d2f8006864ecda7a720
Dominik Szerszeń <dominik.sze@gmail.com>
2014-08-22 00:06:31 +0200

    Solved ClimbingStairs in java

---
+++ ClimbingStairs/Solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int climbStairs(int n) {
+        if(n==0 || n==1) return n;
+        int result = 0;
+        int first  = 1;
+        int second = 1;
+        for(int i=2; i<=n; i++) {
+            result = first+second;
+            first  = second;
+            second = result;
+        }
+        return result;
+    }
+}
