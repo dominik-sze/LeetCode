65030b2d21c1cde5f16434adb1c27d58a1e59dc5
Dominik Szerszen <dominik.sze@gmail.com>
2014-08-26 10:54:49 +0200

    Solved ContainerWithMostWater in java

---
+++ ContainerWithMostWater/Solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length-1;
+        int max = 0;
+        while (left<right) {
+            max = (int)Math.max(Math.min(height[left],height[right])*(right-left), max);
+            if (height[left]<height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+}
